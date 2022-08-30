d5cd81bd8e638b323be259b60e4574a27fc22b02
Jonathan Gagné <jonathan.gagne.1@gmail.com>
2022-08-30 14:40:39 -0400

    Update setup.py
    Added pymysql requirement

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ CLASSIFIERS = [
     "Operating System :: OS Independent",
     "Programming Language :: Python",
 ]
-INSTALL_REQUIRES = ["numpy","pandas","sqlalchemy"]
+INSTALL_REQUIRES = ["numpy","pandas","sqlalchemy","pymysql"]
 SETUP_REQUIRES = [
     "setuptools>=40.6.0",
     "setuptools_scm",
