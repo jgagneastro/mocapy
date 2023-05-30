99f782d482cdf9923bb9b557a80f7df17f30510a
Jonathan Gagne <jonathan.gagne.1@gmail.com>
2023-05-30 15:04:44 -0400

    Added additional sqlalchemy text() wrappers to queries

--- src/mocapy/moca.py
+++ src/mocapy/moca.py
@@ -126,7 +126,7 @@ class MocaEngine:
 		sql_query = sql_query.replace("%","%%")
 
 		#Execute the query
-		df = pd.read_sql(sql_query, active_connection)
+		df = pd.read_sql(text(sql_query), active_connection)
 		
 		#Close the connection unless it is maintained outside of this method
 		if self.connection is None:
